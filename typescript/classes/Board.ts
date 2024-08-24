12d624e3ee61eb904c2624b1328b4d58ff51dcb5
BomanStatic <boman.staticsites@gmail.com>
2024-08-24 08:51:38 +0200

    Descending Diagonal wincheck

--- typescript/classes/Board.ts
+++ typescript/classes/Board.ts
@@ -109,6 +109,19 @@ export default class Board {
     }
 
     // Descending Diagonal
+    for (let row = 3; row < this.matrix.length; row++) {
+      for (let col = 3; col < this.matrix[0].length; col++) {
+        if (
+          this.matrix[row][col] === player.color &&
+          this.matrix[row - 1][col - 1] === player.color &&
+          this.matrix[row - 2][col - 2] === player.color &&
+          this.matrix[row - 3][col - 3] === player.color
+        ) {
+          console.log(`WINNER! WINNER! CHICKEN DINNER!(DESCENDING DIAGONAL, ${player.name} is the winner: ${player.color})`);
+          return player;
+        }
+      }
+    }
     return false;
   }
 }
